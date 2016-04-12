9f7aa16045984f7267f0cfb903359622e439f764
HenrrySalinasJala <Administrator@QDV07DHS1001.fundacion-jala.local>
2016-04-12 16:35:51 -0400

    Print puts variables and operators

---
+++ Henrry/prt_print_puts.rb
@@ -0,0 +1,14 @@
+=begin
+This is my first multi-line comment
+=end
+#-*- coding:utf-8-*-
+print "first line "
+puts "second line "
+print "first line "
+puts "second line "
+print "first line "
+puts "second line "
+#this a single line coment
+
+
+
